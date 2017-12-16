00e5b46106235f093bca5c805976e79d9a03eeca
Enes <oyuncuefsane7@gmail.com>
2017-12-16 15:38:03 +0300

    üssü input la alma

--- b.py
+++ b.py
@@ -1,6 +1,6 @@
 sayi = int(input("Sayı giriniz:\n"))
-
+carpilan = int(input("Kaç üssü olsun ?\n"))
 def ust_al(sayi):
-  return sayi**2
+  return sayi**carpilan
 
 print("{} sayısının karesi {} dır" .format(sayi, ust_al(sayi)))
